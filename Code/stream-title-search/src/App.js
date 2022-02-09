18187898782fb9c0089a0e7eaf838d281f46bbbc
Greg Slowski <gslowski@ualberta.ca>
2022-02-08 21:56:53 -0700

    Added pulling of all game categories

--- Code/stream-title-search/src/App.js
+++ Code/stream-title-search/src/App.js
@@ -11,6 +11,7 @@ function App() {
   useEffect(() => {
     fetchStreams();
     fetchGameIds();
+    fetchGames();
   }, []);
 
   const client_id = 'ldehwfy1d6wdrswfworww073rfxyql';
@@ -35,10 +36,58 @@ function App() {
       });
   };
 
+  const fetchGames = async () => {
+
+    var pagination = '';
+    var options = '';
+
+    await helix.get(`https://api.twitch.tv/helix/games/top?first=100`)
+      .then(async (res) => {
+        for (let i = 0; i < 100; i++) {
+          games.push(res.data.data[i].name);
+          options += '<option value="' + res.data.data[i].name + '" />';
+        }
+        pagination = res.data.pagination.cursor;
+      })
+      .catch(async (err) => {
+        console.log(err);
+      });
+
+    document.getElementById('Categories').innerHTML = options;
+
+    var continueGameLookup = 'yes'
+
+    while (continueGameLookup === 'yes') {
+      await helix.get(`https://api.twitch.tv/helix/games/top?first=100&after=${pagination}`)
+        .then(async (res) => {
+          for (let i = 0; i < 100; i++) {
+            if (typeof (res.data.data[i]) === 'undefined') {
+              continueGameLookup = 'no';
+              break;
+            } else {
+              games.push(res.data.data[i].name);
+              options += '<option value="' + res.data.data[i].name + '" />';
+            }
+          }
+
+          pagination = res.data.pagination.cursor;
+
+        })
+        .catch(async (err) => {
+          if (err.response.status === 429) {
+            continueGameLookup = 'no';
+          }
+          console.log(err);
+        });
+    }
+
+    document.getElementById('Categories').innerHTML = options;
+
+  };
+
   const fetchGameIds = () => {
     helix.get(`https://api.twitch.tv/helix/games?name=World%20of%20Warcraft`)
       .then((res) => {
-        console.log(res.data.data[0].name)
         setGameId(res.data.data[0].name);
       })
       .catch((err) => {
@@ -51,24 +100,34 @@ function App() {
   };
 
   const [streams, setStreams] = useState([]);
+  const [games, setGames] = useState([]);
   const [gameId, setGameId] = useState('');
   const [keywords, setKeywords] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
-          <img src='https://raw.githubusercontent.com/gslowski/AdvancedStreamSearch/main/Images/title.png' width="300" height="40" css={css`margin-left: 4%;`}></img>
+        <img src='https://raw.githubusercontent.com/gslowski/AdvancedStreamSearch/main/Images/title.png' width="300" height="40" css={css`margin-left: 4%;`}></img>
       </header>
 
       <div className="App-background">
         <div css={css`width: 95%; position: absolute; top: 5vh`}>
-          <div className="box" css={css`width: 100%; position: relative; top: 5vh; margin: auto;`}>
-            <input className="input"
-              type="text"
-              placeholder="What interests you today?"
-              css={css`width: 85%`}
-              onChange={searchKeywordHandler}></input>
-            <button className="button is-warning" css={css`width: 15%`}>Search</button>
+          <div className="box" css={css`width: 60%; position: relative; top: 5vh; margin: auto;`}>
+            <div>
+              <input name="GameOrCategory" list="Categories" />
+              <datalist id="Categories"></datalist>
+            </div>
+            <div>
+              <input className="input"
+                id="enteredKeywords"
+                type="text"
+                placeholder="What are you looking for today?"
+                css={css`width: 85%`}></input>
+              <button className="button is-warning" css={css`width: 15%`}>Search</button>
+            </div>
+            <div>
+
+            </div>
           </div>
           <div className="box" css={css`width: 100%; position: relative; top: 2vh; margin: auto;`}>
 
--- Code/stream-title-search/src/Components/Results.js
+++ Code/stream-title-search/src/Components/Results.js
@@ -46,16 +46,27 @@ const Results = ({ streamList }) => {
     const streamElements = (stream) => {
         return (
             <div className="column is-centered">
-                <div>
+                <div className="box" css={css`background-color: #a697af;`}>
+                    <div className="columns is-full" css={css`display: table-cell; vertical-align: middle;`}>
+                        <div className="column has-text-right" css={css`padding: 0; vertical-align: middle;`}>
+                            <a href={"https://www.twitch.tv/" + stream.user_name} >
+                                <img id={stream.user_id} css={css`object-fit: cover; border-radius: 30px;`}>{fetchStreamer(stream.user_id)}</img>
+                            </a>
+                        </div>
+                        <div className="column has-text-left" css={css`padding: 0; vertical-align: middle;`}>
+                            {stream.user_name}
+                        </div>
+                        <div className="column has-text-right" css={css`padding: 0;`}>
+                            <img src='https://raw.githubusercontent.com/gslowski/AdvancedStreamSearch/main/Images/viewers.png' width="30" height="30" css={css`margin-right: 0%;`}></img>
+                        </div>
+                        <div className="column has-text-left" css={css`padding: 0;`}>
+                            {stream.viewer_count}
+                        </div>
+                    </div>
                     <a href={"https://www.twitch.tv/" + stream.user_name}>
-                        <img id={stream.user_id}>{fetchStreamer(stream.user_id)}</img>
+                        <img src={stream.thumbnail_url.replace('{width}', '600').replace('{height}', '360')}></img>
                     </a>
-                    {stream.user_name}
                 </div>
-                <a href={"https://www.twitch.tv/" + stream.user_name}>
-                    <img src={stream.thumbnail_url.replace('{width}', '600').replace('{height}', '360')}></img>
-                </a>
-                <div>Viewers: {stream.viewer_count}</div>
             </div>
         )
     }
@@ -64,7 +75,7 @@ const Results = ({ streamList }) => {
         helix.get(`https://api.twitch.tv/helix/users?id=${user_id}`)
             .then((res) => {
                 document.getElementById(user_id).src = res.data.data[0].profile_image_url;
-                document.getElementById(user_id).style = "width:40px;height:40px;"
+                document.getElementById(user_id).style = "width:30px;height:30px;"
             })
             .catch((err) => {
                 console.log(err);
